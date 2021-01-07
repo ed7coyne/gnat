ce5266f0109e911923913441952560ceb6dc09b0
Ed Coyne <edcoyne@google.com>
2021-01-07 23:36:30 +0000

    Update logging to add two log levels.

--- include/gnat/log.h
+++ include/gnat/log.h
@@ -2,39 +2,47 @@
 
 #ifdef ARDUINO
 
-#ifdef ARDUINO_DEBUG
+#if GNAT_LOG_LEVEL > 0
 
-
-static constexpr size_t kBufferSize = 128;
-static char log_buffer[kBufferSize];
+static constexpr size_t kLogBufferSize = 128;
+static char log_buffer[kLogBufferSize];
 void serial_printf(const char* format, ...) {
   va_list args;
   va_start (args, format);
 
-  int written = vsnprintf(log_buffer, kBufferSize - 1, format, args);
+  int written = vsnprintf(log_buffer, kLogBufferSize - 1, format, args);
   va_end (args);
 
   log_buffer[written] = 0;
   Serial.println(log_buffer);
 }
 
+#define LOG(...) serial_printf( __VA_ARGS__);
+
 #else
 
-#define serial_printf(...) 
+#define LOG(...)
 
 #endif
 
-#define LOG(...) serial_printf( __VA_ARGS__);
+#if GNAT_LOG_LEVEL > 1
+
 #define DEBUG_LOG(...) serial_printf(__VA_ARGS__)
 
-#else 
+#else
+
+#define DEBUG_LOG(...)
+
+#endif
+
+#else // Not Arduino
+
+// Since this is currently only for tests log level is ignored and you get all the logs.
 
 #define LOG(...) printf(__VA_ARGS__);
 #define DEBUG_LOG(...) printf(__VA_ARGS__)
 
 #endif
-//#define LOG(...) printf(__VA_ARGS__);
-//#define DEBUG_LOG(...) printf(__VA_ARGS__)
 
 void LogHex(uint8_t* data, size_t size) {
   for (size_t i = 0; i < size; i++) {
